ec4f8353c06caae50ac1c4329ddb6f219d5517f1
Barrsten <33678642+Barrsten@users.noreply.github.com>
2019-06-07 14:36:56 +0900

    Add files via upload

---
+++ BulletController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BulletController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text scoreText;
+    static int score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+    	if(collision.gameObject.tag == "Enemy")
+        {
+        	Destroy(collision.gameObject);
+        	score=score+1;
+            scoreText.text = "Score:"+score;
+            if(score>=4)
+            {
+                SceneManager.LoadScene(0);
+                score=0;
+            }
+        }
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+	Rigidbody rb;
+    public GameObject bullet;
+    GameObject bulletClone;
+    Rigidbody rbClone;
+    public Text hpText;
+    
+    int hp = 10;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+       transform.Rotate(0, moveHorizontal, 0 * 60f);
+       if(Input.GetKeyDown("space"))
+       {
+          bulletClone = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+          rbClone = bulletClone.GetComponent<Rigidbody>();
+          rbClone.AddForce(transform.forward * 1000f);
+       }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            hp=hp-1;
+            hpText.text = "HP:"+hp;
+            if(hp == 0)
+            {
+              SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
